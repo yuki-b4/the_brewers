623644bafebd9d4ea6f15b0a594b3d034c75403d
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-12 12:12:18 +0900

    投稿内容の保存
    画像、タグに関しては、それぞれの実装フェーズを設ける

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,24 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Articles.order(created_at: :desc)
   end
 
   def new
-    
+    @article = Article.new
   end
 
   def create
-    
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :detail, :how_brew, :why_brew, :commit, :taste).merge(user_id: current_user.id)
   end
 end

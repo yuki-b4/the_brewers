1c7c244137dc108cb828a097d6a4298c6e068a57
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-14 14:52:52 +0900

    コントローラーとルーティングの作成、アソシエーションのの修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   has_many :article_tag_relations
-  has_many :article, through: :article_tag_relation
+  has_many :articles, through: :article_tag_relation
 
   # 一意性の制限は、単体モデルで記述する。また、case_sensitive: falseは、大文字小文字の使い分けをしない設定で、DBのcollationなしでエラーを回避している。
   validates :name, uniqueness: { case_sensitive: false }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
-  resources :articles, only: [:index, :new, :create]
+  resources :articles, only: [:index, :new, :create, :show]
 end

728ef910abd513baf80eab316be4e38da3ae386a
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-15 15:32:37 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
-  resources :articles, only: [:index, :new, :create, :show]
+  resources :articles, only: [:index, :new, :create, :edit, :update, :show]
 end

7650d3188db26e052eced254aa1db3cf2de61cf6
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-18 18:14:57 +0900

    お気に入りの保存・削除機能の実装
    jqueryの導入方法が変更になっていたので、gemから削除しwebpackerを利用した。

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,14 @@ class FavoritesController < ApplicationController
   end
 
   def create
+    @article = Article.find(params[:article_id])
     favorite = current_user.favorites.build(article_id: params[:article_id])
     favorite.save
-    redirect_to articles_path
   end
 
   def destroy
+    @article = Article.find(params[:article_id])
     favorite = Favorite.find_by(article_id: params[:article_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to articles_path
   end
 end

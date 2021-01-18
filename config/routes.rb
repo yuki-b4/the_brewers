9100f54e1c5f81913c7b71e1f3ebb08045bce759
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-18 15:31:28 +0900

    保存機能と削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
   resources :articles do
-    resources :favorites, only: [:index, :create]
+    resource :favorites, only: [:create, :destroy]
     collection do
       get "search"
     end

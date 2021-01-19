d9be3cf020821dc364ff3dce2e249442a8832766
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-19 16:12:40 +0900

    お気に入り一覧表示の並び替え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def bookmark_list
-    @favorite_articles = current_user.favorite_articles
+    @favorite_articles = current_user.favorite_articles.order("favorites.created_at desc")
   end
 end

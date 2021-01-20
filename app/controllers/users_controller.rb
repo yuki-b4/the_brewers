a7b9cc44d9410c25cbb6ed4929644c78a8e86d02
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-20 11:28:21 +0900

    ユーザー詳細情報表示機能の実装とページバリデーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :move_to_index, only: [:show]
+
+  def show
+    @user = User.find(params[:id])
+    @user_articles = @user.articles
+  end
 
   def bookmark_list
     @favorite_articles = current_user.favorite_articles.order('favorites.created_at desc')
+  end
+
+  private
+  def move_to_index
+    @user = User.find(params[:id])
+    unless current_user.id == @user.id
+      redirect_to root_path
+    end
   end
 end

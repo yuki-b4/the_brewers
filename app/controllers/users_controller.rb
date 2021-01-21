569224197137d6afe2208503d6e48ecabe00b51f
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-21 12:24:35 +0900

    rubocopの実行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,9 @@ class UsersController < ApplicationController
   end
 
   private
+
   def move_to_index
     @user = User.find(params[:id])
-    unless current_user.id == @user.id
-      redirect_to root_path
-    end
+    redirect_to root_path unless current_user.id == @user.id
   end
 end

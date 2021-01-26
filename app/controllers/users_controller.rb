853434ea599d12520d649716aeda06322c1056be
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-01-26 14:15:18 +0900

    articles#index/searchを部分テンプレート化
    # What
    articles#index/searchの部分テンプレート化
    userの詳細情報へのリンクを指定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:show, :move_to_index]
-  before_action :move_to_index, only: [:show]
 
   def show
+    @user = User.find(params[:id])
     @user_articles = @user.articles
   end
 
@@ -11,13 +10,4 @@ class UsersController < ApplicationController
     @favorite_articles = current_user.favorite_articles.order('favorites.created_at desc')
   end
 
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def move_to_index
-    redirect_to root_path unless current_user.id == @user.id
-  end
 end

843cebab27bb248b250c67188b4feb1ce201622e
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2021-02-13 17:45:37 +0900

    ビューにおけるフォロー処理ボタンの実装と、relationshipsテーブルへの保存

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: true
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follow_id"
   has_many :followers, through: :reverse_of_relationships, source: :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -29,12 +29,13 @@ class User < ApplicationRecord
     end
   end
 
-  def unfollow
-    realtionship = self.relationships.find_by(follow_id: other_user.id)
-    realtionship.destroy if realtionship
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
   end
 
   def following?(other_user)
-    self.followings.include(other_user)
+    self.followings.include?(other_user)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
       get "search"
     end
   end
-  resources :relationships, only: [:create, :destroy]
+  resources :relationships, only: [:create,  :destroy]
 end
